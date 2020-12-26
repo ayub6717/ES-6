38dcb67ebb68cc2b2f37f74ae8f34e477bff4827
Md. Ayub Shamim <ayub15-6717@diu.edu.bd>
2020-12-26 19:24:21 +0600

    template string

---
+++ src/Template_String.js
@@ -0,0 +1,10 @@
+const name = 'MD AYUB SHAMIM';
+const age = 24;
+const email = 'ahmed1.ayub6717@gmail.com';
+
+// const details = 'Name: ' + name + 'Age: ' + age + 'Email: ' + email
+/* this is previous style and very annoying */
+
+const details = `Name: ${name}; Age: ${age}; Email: ${email}`
+console.log(details);
+/* Easy  to concat that's why use template string*/
