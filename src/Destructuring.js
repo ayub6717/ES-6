3dbe0a9693f2eea1d3bc8c7d6125c2aa67bcdb2a
ayub6717 <ayub15-6717@diu.edu.bd>
2020-12-29 00:18:42 +0600

    Destructuring

--- src/Destructuring.js
+++ src/Destructuring.js
@@ -1,10 +1,3 @@
-const arr = [19, 20, 32]; 
-let [a, b, c] = arr;
-console.log(a);
-console.log(c);
-
-//==================
-
 const obj = {
     Name: 'MD AYUB SHAMIM',
     Email: 'ahmed1.ayub6717@gmail.com'
@@ -16,3 +9,10 @@ function print({Name, Email}){
     console.log(`Hello ${Name}; Email: ${Email}`);
 }
 print({Email,Name});
+
+//==================
+
+const arr = [19, 20, 32]; 
+let [a, b, c] = arr;
+console.log(a);
+console.log(c);
