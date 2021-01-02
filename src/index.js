f4f489ed4a87cacb76f2a81007919540c2ce9076
ayub6717 <ayub15-6717@diu.edu.bd>
2021-01-02 18:39:34 +0600

    Classes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import * as Module_system from './Module-system';
 import * as Spread_operator from './Spread-operator';
 import {add, mod} from './Module-system';
+import Person from './Classes';
 
 console.log(Module_system.sub(10, 20));
 console.log(mod(10, 3));
 console.log(Spread_operator.obj2);
+
+let p1 = new Person('MD. AYUB', 'ahmed1.ayub6717@gmail.com');
+console.log(p1.name);
+p1.print();
 
