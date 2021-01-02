8733cf6ae17217025c745cd11137a9667003c992
ayub6717 <ayub15-6717@diu.edu.bd>
2021-01-02 19:05:58 +0600

    Template String

--- src/Template_String.js
+++ src/Template_String.js
@@ -2,11 +2,12 @@ const name = 'MD AYUB SHAMIM';
 const age = 24;
 const email = 'ahmed1.ayub6717@gmail.com';
 const address = 'Dhaka';
+const id = '6717';
 
 // const details = 'Name: ' + name + 'Age: ' + age + 'Email: ' + email
 /* this is previous style and very annoying */
 
-const details = `Name: ${name}; Age: ${age}; Email: ${email}; Address: ${address}`
+const details = `Name: ${name}; Age: ${age}; Email: ${email}; Address: ${address}; Id: ${id}`
 console.log(details);
 /* Easy  to concat that's why use template string*/
 
